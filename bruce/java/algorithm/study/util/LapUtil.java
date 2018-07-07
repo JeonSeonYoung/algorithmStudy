c3684bcc160a75140262664beb2513ad49010e5a
Bruce Dae-yeong Kim <dykim1983@gmail.com>
2018-07-07 11:56:44 +0900

    expand laputil

--- bruce/java/algorithm/study/util/LapUtil.java
+++ bruce/java/algorithm/study/util/LapUtil.java
@@ -5,13 +5,19 @@ import java.util.function.Supplier;
 
 public class LapUtil {
     //private static StopWatch stopWatch = new StopWatch();
+    private static volatile long executeIndex = 0L;
 
-    public static void check(Runnable r) {
+    public static void check(String message, Runnable r) {
         StopWatch stopWatch = new StopWatch();
         //stopWatch.reset();
         stopWatch.start();
         r.run();
+        System.out.println(message);
         System.out.println("elapsed time : " + stopWatch.getTime(TimeUnit.MILLISECONDS));
+    }
+
+    public static void check(Runnable r) {
+        check("executing method " + executeIndex++, r);
     }
 
 
